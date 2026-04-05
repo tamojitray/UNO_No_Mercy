6c23631f27042a15d89ad4d77ce483a8d02f0b44
tamojitray <tamojitray.tr.2000@gmail.com>
2026-04-05 15:54:06 +0530

    fix connection port bug

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE = window.location.protocol + "//" + window.location.hostname + ":8000";
+const API_BASE = import.meta.env.PROD 
+  ? window.location.origin 
+  : window.location.protocol + "//" + window.location.hostname + ":8000";
 
 export default function Home({ setView, setRoomCode, setUsername, setSessionToken }) {
   const [name, setName] = useState('');
--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { socket } from '../socket';
 
-const API_BASE = window.location.protocol + "//" + window.location.hostname + ":8000";
+const API_BASE = import.meta.env.PROD 
+  ? window.location.origin 
+  : window.location.protocol + "//" + window.location.hostname + ":8000";
 
 export default function Room({ roomCode, username, sessionToken, setView }) {
   const [players, setPlayers] = useState([]);
